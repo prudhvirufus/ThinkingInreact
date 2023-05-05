98756b60c2f423ed3647b1c7f89c64fca17f301b
prudhvirufus <prudhvirufus@gmail.com>
2023-05-05 14:25:02 +0530

    Example code for the Thinking in react

---
+++ thinikginreact/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ThikingInReact from "./ThinkingInReact";
+
+function App() {
+  return (
+    <div>
+      <ThikingInReact />
+    </div>
+  );
+}
+
+export default App;
